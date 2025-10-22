bbdfa1351fc99371245991f425724df699693267
Kirill Evdokimov <kirillprof@yandex.ru>
2025-10-23 01:21:46 +0300

    refactor(docker): improve DockerfileProduction and entrypoint script
    - Updated COPY instruction syntax for cmd.sh to use JSON array format
    - Simplified cron setup by removing redundant mkdir and touch commands
    - Adjusted permissions for supervisord logging directory to support non-root execution- Modified entrypoint script to keep root privileges for supervisord while allowing programs to drop to www-data
    - Removed unnecessary su-exec usage in entrypoint script- Ensured proper log file permissions for cron and supervisord

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 set -e
 
-# If running as root, start cron and drop privileges to www-data
+# If running as root, start cron and keep root for supervisord (programs drop to www-data)
 if [ "$(id -u)" = '0' ]; then
     # Start cron in the background
     /usr/sbin/crond -L /var/log/cron/cron.log &
-    # Re-exec this script as www-data to run the final CMD
-    exec su-exec www-data "$0" "$@"
 fi
 
-# Running as non-root: execute the CMD from the Dockerfile
+# Execute the CMD from the Dockerfile (supervisord will run as root; programs use user=www-data)
 exec "$@"
