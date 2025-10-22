11009e1070609774aaf1a5eadbdcaffc11c8c9c1
Kirill Evdokimov <kirillprof@yandex.ru>
2025-10-23 00:38:21 +0300

    refactor(docker): restructure production deployment and container management
    - Remove redundant environment variables and user configurations from compose file- Update deployment workflow to copy entire project and use production compose file
    - Modify Dockerfile to use root user with entrypoint privilege dropping- Refactor entrypoint script to properly manage cron and user privileges
    - Configure supervisord to manage php-fpm and messenger consumer processes
    - Simplify container startup by removing manual user switching
    - Improve log handling for supervised processes

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-# Start cron in the background as root
-/usr/sbin/crond -f -L /var/log/cron/cron.log &
-
-# Switch to www-data user if not already
+# If running as root, start cron and drop privileges to www-data
 if [ "$(id -u)" = '0' ]; then
+    # Start cron in the background
+    /usr/sbin/crond -L /var/log/cron/cron.log &
+    # Re-exec this script as www-data to run the final CMD
     exec su-exec www-data "$0" "$@"
 fi
 
-# Execute the CMD from the Dockerfile
+# Running as non-root: execute the CMD from the Dockerfile
 exec "$@"
