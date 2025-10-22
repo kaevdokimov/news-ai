27958f1b03a76d9a9a23f5d10208b64ef538bfb8
Kirill Evdokimov <kirillprof@yandex.ru>
2025-10-23 00:23:28 +0300

    chore(deps): update dependencies
    - Update rector/rector to version 2.2.4
    - Updated rector/rector from version 2.2.3 to 2.2.4
    - Updated roave/security-advisories to latest dev version
    - Added conflict rule for code16/sharp <9.7- Updated admidio/admidio conflict rule to <=4.3.16- Added Symfony DebugBundle and WebProfilerBundle to bundles configuration

--- config/bundles.php
+++ config/bundles.php
@@ -1,35 +1,20 @@
 <?php
 
-declare(strict_types=1);
-
-use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
-use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
-use Symfony\Bundle\TwigBundle\TwigBundle;
-use Symfony\UX\StimulusBundle\StimulusBundle;
-use Symfony\UX\Turbo\TurboBundle;
-use Twig\Extra\TwigExtraBundle\TwigExtraBundle;
-use Symfony\Bundle\SecurityBundle\SecurityBundle;
-use Symfony\Bundle\MonologBundle\MonologBundle;
-use Symfony\Bundle\MakerBundle\MakerBundle;
-use Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle;
-use Knp\Bundle\MenuBundle\KnpMenuBundle;
-use Sonata\AdminBundle\SonataAdminBundle;
-use Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle;
-
 return [
-    FrameworkBundle::class => ['all' => true],
-    DoctrineBundle::class => ['all' => true],
-    DoctrineMigrationsBundle::class => ['all' => true],
-    TwigBundle::class => ['all' => true],
-    StimulusBundle::class => ['all' => true],
-    TurboBundle::class => ['all' => true],
-    TwigExtraBundle::class => ['all' => true],
-    SecurityBundle::class => ['all' => true],
-    MonologBundle::class => ['all' => true],
-    MakerBundle::class => ['dev' => true],
-    DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    KnpMenuBundle::class => ['all' => true],
-    SonataAdminBundle::class => ['all' => true],
-    SonataDoctrineORMAdminBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
+    Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
